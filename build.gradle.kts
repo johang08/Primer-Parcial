d49607f13be1b07616ea4988171a193c8a653a5f
johang08 <johangonzalez986@gmail.com>
2025-06-04 17:56:11 -0400

    Agrega Dependencias

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-
+//dependencias
     buildscript {
         dependencies {
             classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
